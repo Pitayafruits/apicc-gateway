2b10c616cbe56d60fe199e73080ebf607f17f405
CC <112964687+Pitayafruits@users.noreply.github.com>
2024-10-19 22:20:48 +0800

    feat：构建启动类

---
+++ gateway-core/src/main/java/com/apicc/core/Bootstrap.java
@@ -0,0 +1,22 @@
+package com.apicc.core;
+
+/**
+ * API网关启动类.
+ */
+public class Bootstrap
+{
+    public static void main( String[] args ) {
+        //1.加载网关核心静态配置
+
+        //2.插件初始化
+
+        //3.配置中心初始化-连接配置中心，监听配置中心的新增、修改、删除
+
+        //4.启动容器
+
+        //5.注册中心，将注册中心的实例加载到本地
+
+        //6.服务优雅关机
+
+    }
+}
